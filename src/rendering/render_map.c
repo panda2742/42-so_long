346af14439735a01f5fc5c55bb2a85b454786548
elouannh <elouannhosta27@gmail.com>
2025-02-27 16:40:31 +0100

    Rendering game

--- src/rendering/render_map.c
+++ src/rendering/render_map.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_game.c                                        :+:      :+:    :+:   */
+/*   render_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehosta <ehosta@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/27 09:43:19 by ehosta            #+#    #+#             */
-/*   Updated: 2025/02/27 16:33:50 by ehosta           ###   ########.fr       */
+/*   Created: 2025/02/27 16:34:03 by ehosta            #+#    #+#             */
+/*   Updated: 2025/02/27 16:39:35 by ehosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	init_game(t_sl *sl)
+void	render_map(t_sl *sl)
 {
 	t_game	g;
-	int		i;
 
 	g = sl->game;
-	g.mlx = mlx_init();
-	g.win = mlx_new_window(
-			g.mlx,
-			sl->width * 64,
-			sl->height * 64,
-			"so_long LES SUSHIS ARRIVENT");
-	i = -1;
-	while (++i < XPM_IMAGES)
-		g.imgs[i].ptr = mlx_xpm_file_to_image(
-				g.mlx, get_xpm()[i], &g.imgs[i].w, &g.imgs[i].h);
-	mlx_loop(g.mlx);
-	return (sl->status);
 }
